d62858517499427c620b2b4334a709fa78350b9c
Vicente Cortes <vicenteicc2008@gmail.com>
2024-06-17 11:07:44 -0400

    remove gzip

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p RedmiNote7Pkg/RedmiNote7Pkg.dsc
-gzip -c < workspace/Build/RedmiNote7Pkg/DEBUG_GCC5/FV/REDMINOTE7PKG_UEFI.fd >uefi_image
-cat troika.dtb >>uefi_image
+cat workspace/Build/RedmiNote7Pkg/DEBUG_GCC5/FV/REDMINOTE7PKG_UEFI.fd troika.dtb >>uefi_image
